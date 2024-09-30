bf772fb0b1fdfc45a4ac8d510c23f4ac2b496add
TeknoMaven <teknomaven.krisna@gmail.com>
2024-09-30 09:58:08 +0700

    add slider

---
+++ slider/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  let currentIndex = 0;
+  const slides = $('.slide');
+  const totalSlides = slides.length;
+
+  $('.next').click(function () {
+      currentIndex = (currentIndex + 1) % totalSlides;
+      updateSlider();
+  });
+
+  $('.prev').click(function () {
+      currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+      updateSlider();
+  });
+
+  function updateSlider() {
+      const newMargin = -currentIndex * 100 + '%';
+      $('.slides').css('margin-left', newMargin);
+  }
+});
